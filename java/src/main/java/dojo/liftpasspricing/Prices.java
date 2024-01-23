3626f7b4bec64a293f2dfac6a80b407c129ebf70
Adrián Lizaga <lizagaadrian@gmail.com>
2024-01-23 19:51:21 +0100

    refactor: remove reduntant and

--- java/src/main/java/dojo/liftpasspricing/Prices.java
+++ java/src/main/java/dojo/liftpasspricing/Prices.java
@@ -105,7 +105,7 @@ public class Prices {
                                 }
                             }
                         } else {
-                            if (age != null && age >= 6) {
+                            if (age != null) {
                                 if (age > 64) {
                                     return "{ \"cost\": " + (int) Math.ceil(result.getInt("cost") * .4) + "}";
                                 } else {
