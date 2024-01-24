1555e7577f5a9a16e39fe12e2fa47f9de8b86e3d
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2024-01-24 19:02:53 +0100

    refactor: extract methods to staycalculator class

---
+++ java/src/main/java/dojo/liftpasspricing/StayCalculator.java
@@ -0,0 +1,100 @@
+package dojo.liftpasspricing;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class StayCalculator{
+public static String calculateCost(
+      Integer age, Connection connection, int baseCost, String date, String stayType)
+      throws ParseException, SQLException {
+    if (isChild(age)) {
+      return buildCost(0);
+    }
+
+    if (!stayType
+        .equals("night")) {
+      return calculateOneJourCost(connection, age, baseCost, date);
+    }
+    return calculateNightCost(age, baseCost);
+  }
+  private static String calculateOneJourCost(
+      Connection connection, Integer age, int baseCost, String date)
+      throws ParseException, SQLException {
+    DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    int reduction = calculateReduction(isoFormat, isHoliday(connection, isoFormat,
+                                                            date), date);
+
+    // TODO apply reduction for others
+    if (isTeenager(age)) {
+      return buildCost((int) Math.ceil(baseCost * .7));
+    }
+    if (age == null) {
+      double cost = baseCost * (1 - reduction / 100.0);
+      return buildCost((int) Math.ceil(cost));
+    }
+    if (isSenior(age)) {
+      double cost = baseCost * .75 * (1 - reduction / 100.0);
+      return buildCost((int) Math.ceil(cost));
+    }
+    double cost = baseCost * (1 - reduction / 100.0);
+    return buildCost((int) Math.ceil(cost));
+  }private static String calculateNightCost(Integer age, int baseCost) throws SQLException {
+    if (age == null) {
+      return buildCost(0);
+    }
+    if (isSenior(age)) {
+      return buildCost((int) Math.ceil(baseCost * .4));
+    }
+    return buildCost(baseCost);
+  }private static boolean isTeenager(Integer age) {
+    return age != null && age < 15;
+  }private static boolean isChild(Integer age) {
+    return age != null && age < 6;
+  }private static boolean isHoliday(Connection connection, DateFormat isoFormat, String date)
+      throws SQLException, ParseException {
+    boolean isHoliday = false;
+    try (PreparedStatement holidayStmt =
+        connection.prepareStatement( //
+            "SELECT * FROM holidays")) {
+      try (ResultSet holidays = holidayStmt.executeQuery()) {
+
+        while (holidays.next()) {
+          Date holiday = holidays.getDate("holiday");
+          if (date != null) {
+            Date d = isoFormat.parse(date);
+            if (d.getYear() == holiday.getYear()
+                && //
+                d.getMonth() == holiday.getMonth()
+                && //
+                d.getDate() == holiday.getDate()) {
+              isHoliday = true;
+            }
+          }
+        }
+      }
+    }
+    return isHoliday;
+  }private static int calculateReduction(DateFormat isoFormat, boolean isHoliday, String date)
+      throws ParseException {
+    int reduction = 0;
+    if (date != null) {
+      Calendar calendar = Calendar.getInstance();
+      calendar.setTime(isoFormat.parse(date));
+      if (!isHoliday && calendar.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
+        reduction = 35;
+      }
+    }
+    return reduction;
+  }private static boolean isSenior(Integer age) {
+    return age > 64;
+  }private static String buildCost(int cost) {
+    return "{ \"cost\": " + cost + "}";
+  }}
